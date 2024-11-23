39e8fe3f0636092057e2cad04b7789297586045c
Gustavo Carvalho <gustavocarvalho.gc79@gmail.com>
2024-11-23 18:49:44 -0300

    commit subindo atualização de validação do formulário (telefone e cpf formatados)

--- Transparencia_Iluminada/src/app/Components/forms/forms.component.ts
+++ Transparencia_Iluminada/src/app/Components/forms/forms.component.ts
@@ -5,8 +5,8 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-forms',
-  standalone: true, // Declara o componente como standalone
-  imports: [CommonModule, ReactiveFormsModule, HttpClientModule], // Dependências necessárias
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.css']
 })
@@ -18,13 +18,53 @@ export class FormsComponent {
     this.formulario = this.fb.group({
       nome: ['', Validators.required],
       cpf: ['', [Validators.required, Validators.pattern(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/)]],
-      telefone: ['', Validators.required],
+      telefone: ['', [Validators.required, Validators.pattern(/^\(\d{2}\)9\d{4}-\d{4}$/)]],
       logradouro: ['', Validators.required],
       referencia: [''],
       tipoLampada: ['', Validators.required],
       tipoProblema: ['', Validators.required],
     });
   }
+
+  /**
+   * Formata o CPF conforme o usuário digita.
+   * Exemplo: "12345678909" -> "123.456.789-09"
+   */
+  formatCpf(event: Event) {
+    const input = event.target as HTMLInputElement;
+    let value = input.value.replace(/\D/g, ''); // Remove tudo que não for número
+
+    if (value.length > 3) {
+      value = value.replace(/^(\d{3})(\d)/, '$1.$2');
+    }
+    if (value.length > 6) {
+      value = value.replace(/^(\d{3})\.(\d{3})(\d)/, '$1.$2.$3');
+    }
+    if (value.length > 9) {
+      value = value.replace(/^(\d{3})\.(\d{3})\.(\d{3})(\d)/, '$1.$2.$3-$4');
+    }
+
+    input.value = value; // Atualiza o valor do input
+    this.formulario.controls['cpf'].setValue(value); // Atualiza o valor no formulário
+  }
+/**
+   * Formata o telefone conforme o usuário digita.
+   * Exemplo: "11999999999" -> "(11)99999-9999"
+   */
+formatTelefone(event: Event) {
+  const input = event.target as HTMLInputElement;
+  let value = input.value.replace(/\D/g, ''); // Remove tudo que não for número
+
+  if (value.length > 2) {
+    value = value.replace(/^(\d{2})(\d)/, '($1)$2'); // Adiciona os parênteses ao DDD
+  }
+  if (value.length > 7) {
+    value = value.replace(/(\d{5})(\d{4})/, '$1-$2'); // Adiciona o traço no número
+  }
+
+  input.value = value; // Atualiza o valor do input
+  this.formulario.controls['telefone'].setValue(value); // Atualiza o valor no formulário
+}
 
   onSubmit() {
     if (this.formulario.valid) {
--- Transparencia_Iluminada/src/app/Pages/home/home.component.ts
+++ Transparencia_Iluminada/src/app/Pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { BtnReportarComponent } from '../../Components/btn-reportar/btn-reportar.component';
+import { FormsComponent } from "../../Components/forms/forms.component";
 
 @Component({
   selector: 'app-home',
-  imports: [BtnReportarComponent],
+  imports: [BtnReportarComponent, FormsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
